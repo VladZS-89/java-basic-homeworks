1075a10c668e0c1493129fe09bb425395e6d7d1b
Vlad <zhuravlevv89@mail.ru>
2024-02-21 01:04:03 +0300

    homework14 fix2

--- src/main/java/ru/zhuravlev/homeworks/homework14/MainApp.java
+++ src/main/java/ru/zhuravlev/homeworks/homework14/MainApp.java
@@ -1,5 +1,8 @@
 package ru.zhuravlev.homeworks.homework14;
 
+import ru.zhuravlev.homeworks.homework14.exeption.AppArrayDataException;
+import ru.zhuravlev.homeworks.homework14.exeption.AppArraySizeException;
+
 public class MainApp {
     static String[][] strings1, strings2, strings3;
 
@@ -13,20 +16,34 @@ public class MainApp {
         strings2 = new String[][]{
                 {"40", "60", "20", "70"},
                 {"20", "30", "90", "50"},
-                {"30", "80", "10", "60"},
+                {"30", "80", "10", "10", "60"},
                 {"10", "20", "90", "80"}
         };
         strings3 = new String[][]{
                 {"4", "6", "2", "7"},
-                {"2", "3", "9", "EPIC FAIL"},
+                {"2", "3", "9", "EPIC FAIl"},
                 {"3", "8", "1", "6"},
                 {"1", "2", "9", "8"}
         };
     }
 
     public static void main(String[] args) {
-        ArrayStringService.convertToIntAndSum(strings1);
-        ArrayStringService.convertToIntAndSum(strings2);
-        ArrayStringService.convertToIntAndSum(strings3);
+        try {
+            ArrayStringService.convertToIntAndSum(strings1);
+        } catch (AppArrayDataException | AppArraySizeException exception) {
+            System.out.println("Ошибка " + exception);
+        } finally {
+            try {
+                ArrayStringService.convertToIntAndSum(strings2);
+            } catch (AppArrayDataException | AppArraySizeException exception) {
+                System.out.println("Ошибка " + exception);
+            } finally {
+                try {
+                    ArrayStringService.convertToIntAndSum(strings3);
+                } catch (AppArrayDataException | AppArraySizeException exception) {
+                    System.out.println("Ошибка " + exception);
+                }
+            }
+        }
     }
 }
