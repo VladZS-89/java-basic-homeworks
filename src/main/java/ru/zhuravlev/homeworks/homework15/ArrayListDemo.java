6edcd3f5ffd5b05a37dbfe524e3cc73c1a1d4bdc
zhuravlev_vs <zhuravlev_vs@magnit.ru>
2024-03-04 02:34:25 +0300

    homework15

--- src/main/java/ru/zhuravlev/homeworks/homework15/ArrayListDemo.java
+++ src/main/java/ru/zhuravlev/homeworks/homework15/ArrayListDemo.java
@@ -1,9 +1,20 @@
 package ru.zhuravlev.homeworks.homework15;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+/**
+ * Класс для теста свойств списка ArrayList
+ */
 public class ArrayListDemo {
+    /**
+     * Метод создаёт сортированный по возрастанию список чисел с шагом 1 в диапазоне заданных значений.
+     *
+     * @param min минимальное целое число диапазона
+     * @param max максимальное целое число диапазона
+     * @return сортированный по возрастанию список чисел с шагом 1
+     */
     public static List<Integer> sort(int min, int max) {
         List<Integer> sortedIntList = new ArrayList<>();
         if (min > max) {
@@ -17,7 +28,14 @@ public class ArrayListDemo {
         return sortedIntList;
     }
 
+    /**
+     * Метод суммирующий числа из списка, которые больше 5
+     *
+     * @param integerList список целых чисел
+     * @return сумма чисел из списка, которые больше 5
+     */
     public static long sumListValuesIfMoreThenFive(List<Integer> integerList) {
+        System.out.print("Список целых чисел: " + integerList + "\nСумма чисел, которые больше 5 = ");
         long sum = 0;
         for (Integer i : integerList) {
             if (i > 5) {
@@ -25,5 +43,120 @@ public class ArrayListDemo {
             }
         }
         return sum;
+    }
+
+    /**
+     * Метод заменяет все элементы целочисленного массива на заданное целое число
+     *
+     * @param n       целое число
+     * @param intList список целых чисел
+     */
+    public static void fillingArrayByNumber(int n, List<Integer> intList) {
+        if (intList.isEmpty()) {
+            System.out.println("Передан пустой список");
+            return;
+        }
+        System.out.println(intList);
+        for (int i = 0; i < intList.size(); i++) {
+            intList.set(i, n);
+        }
+        System.out.println(intList);
+    }
+
+    /**
+     * Метод увеличивает каждый элемент целочисленного массива на заданное целое число
+     *
+     * @param n       целое число
+     * @param intList список целых чисел
+     */
+    public static void increaseArrayValuesByNumber(int n, List<Integer> intList) {
+        System.out.print("Заданный массив: " + intList);
+        if (!intList.isEmpty()) {
+            for (int i = 0; i < intList.size(); i++) {
+                intList.set(i, intList.get(i) + n);
+            }
+            System.out.println(". Увеличили каждый элемент на " + n + ". Получили: " + intList);
+        } else {
+            System.out.println("Передан пустой список");
+        }
+    }
+
+    /**
+     * Метод формирует список имён сотрудников
+     *
+     * @param employees список сотрудников
+     * @return список имён сотрудников
+     */
+    public static List<String> getEmployeeNames(List<Employee> employees) {
+        if (!employees.isEmpty()) {
+            List<String> nameList = new ArrayList<>();
+            for (Employee employee : employees) {
+                nameList.add(employee.getName());
+            }
+            return nameList;
+        } else {
+            System.out.println("Список сотрудников пуст");
+            return Collections.emptyList();
+        }
+    }
+
+    /**
+     * Метод возвращает список сотрудников, возраст которых >= заданному
+     *
+     * @param employees список сотрудников
+     * @param minAge    минимальный возраст
+     * @return список сотрудников не менее минимального возраста
+     */
+    public static List<Employee> getEmployeesFilteredByAge(List<Employee> employees, int minAge) {
+        if (employees.isEmpty() || minAge < 0) {
+            throw new RuntimeException("В метод getEmployeesFilteredByAge переданы некорректные параметры");
+        }
+        List<Employee> employeesFilteredByAge = new ArrayList<>();
+        for (Employee employee : employees) {
+            if (employee.getAge() >= minAge) {
+                employeesFilteredByAge.add(employee);
+            }
+        }
+        return employeesFilteredByAge;
+    }
+
+    /**
+     * Метод проверяет не превышает ли средний возраст сотрудников указанный минимальный
+     *
+     * @param employees     список сотрудников
+     * @param minAverageAge минимальный средний возраст
+     * @return true, если средний возраст сотрудников больше указанного; false, если - меньше
+     */
+    public static boolean isMoreAverageMinAge(List<Employee> employees, int minAverageAge) {
+        if (employees.isEmpty() || minAverageAge < 0) {
+            throw new RuntimeException("В метод isMoreAverageMinAge переданы некорректные параметры");
+        }
+        int sumAge = 0;
+        for (Employee employee : employees) {
+            sumAge += employee.getAge();
+        }
+        int averageAge = sumAge / employees.size();
+        return averageAge > minAverageAge;
+    }
+
+    /**
+     * Метод возвращает самого молодого сотрудника из списка
+     *
+     * @param employees список сотрудников
+     * @return самый молодой сотрудник
+     */
+    public static Employee getYoungestEmployee(List<Employee> employees) {
+        if (employees.isEmpty()) {
+            throw new RuntimeException("В метод getYoungestEmployee переданы некорректные параметры");
+        }
+        Employee youngestEmployee = null;
+        int minAge = Integer.MAX_VALUE;
+        for (Employee employee : employees) {
+            if (employee.getAge() < minAge) {
+                minAge = employee.getAge();
+                youngestEmployee = employee;
+            }
+        }
+        return youngestEmployee;
     }
 }
--- src/main/java/ru/zhuravlev/homeworks/homework15/MainApp.java
+++ src/main/java/ru/zhuravlev/homeworks/homework15/MainApp.java
@@ -1,10 +1,58 @@
 package ru.zhuravlev.homeworks.homework15;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println(ArrayListDemo.sort(-3, 6));
-        System.out.println(ArrayListDemo.sumListValuesIfMoreThenFive(List.of(3, 4, 5, 7, 9, 11)));
+        System.out.println(ArrayListDemo.sort(rndInt(20), rndInt(20)));
+        System.out.println(ArrayListDemo.sumListValuesIfMoreThenFive(rndIntegers(10)));
+        List<Integer> integerList = rndIntegers(10);
+        ArrayListDemo.fillingArrayByNumber(5, integerList);
+        ArrayListDemo.increaseArrayValuesByNumber(5, integerList);
+
+
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee("Николай", 55));
+        employeeList.add(new Employee("Людмила", 44));
+        employeeList.add(new Employee("Виктор", 33));
+        employeeList.add(new Employee("Александра", 22));
+
+        System.out.println(ArrayListDemo.getEmployeeNames(employeeList));
+
+        try {
+            System.out.println(ArrayListDemo.getEmployeesFilteredByAge(employeeList, 33));
+        } catch (RuntimeException e) {
+            System.out.println(e);
+        }
+
+        int minAverageAge = 35;
+        try {
+            if (ArrayListDemo.isMoreAverageMinAge(employeeList, minAverageAge)) {
+                System.out.println("Средний возраст сотрудников превышает " + minAverageAge + "!");
+            } else {
+                System.out.println("Средний возраст сотрудников меньше " + minAverageAge + "!");
+            }
+        } catch (RuntimeException e) {
+            System.out.println(e);
+        }
+
+        try {
+            System.out.println("Самый молодой сотрудник - " + ArrayListDemo.getYoungestEmployee(employeeList));
+        } catch (RuntimeException e) {
+            System.out.println(e);
+        }
+    }
+
+    private static int rndInt(int x) {
+        return ((int) (Math.random() * x)) - (x / 2);
+    }
+
+    private static List<Integer> rndIntegers(int x) {
+        List<Integer> integers = new ArrayList<>();
+        for (int i = 0; i < x; i++) {
+            integers.add(rndInt(x * 10));
+        }
+        return integers;
     }
 }
