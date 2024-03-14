778959b0c4de481e64e04792992fbc7b3860ca4f
zhuravlev_vs <zhuravlev_vs@magnit.ru>
2024-03-14 16:37:23 +0300

    homework16

---
+++ src/main/java/ru/zhuravlev/homeworks/homework16/MainApp.java
@@ -0,0 +1,21 @@
+package ru.zhuravlev.homeworks.homework16;
+
+import java.util.HashMap;
+
+public class MainApp {
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook(new HashMap<>());
+
+        phoneBook.add("Журавлев Владислав", 84957773322L);
+        System.out.println(phoneBook.find("Журавлев Владислав "));
+        phoneBook.add(" Журавлев Владислав ", 84957773322L);
+        phoneBook.add("  Журавлев Владислав  ", 84959998877L);
+        phoneBook.add("Кузьмин Сергей  ", 84951115599L);
+        System.out.println(phoneBook.find("журавлев владислав"));
+        if (phoneBook.containsPhoneNumber(84959998877L)) {
+            System.out.println("Телефон есть в справочнике");
+        } else {
+            System.out.println("Телефон отсутствует в справочнике");
+        }
+    }
+}
