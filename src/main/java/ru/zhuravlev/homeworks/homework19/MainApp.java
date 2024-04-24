1a10d17816ab664813459a6fa1c5ef279a2204da
Vlad <zhuravlevv89@mail.ru>
2024-04-24 23:16:07 +0300

    homework19 fix

--- src/main/java/ru/zhuravlev/homeworks/homework19/MainApp.java
+++ src/main/java/ru/zhuravlev/homeworks/homework19/MainApp.java
@@ -8,24 +8,52 @@ import java.util.Scanner;
 public class MainApp {
     public static void main(String[] args) {
         String rootPath = "src";
-        System.out.println("Список файлов корневого каталога:");
-        File file = new File(rootPath);
-
-        for (File f : Objects.requireNonNull(file.listFiles())) {
-            System.out.println("f.toString() = " + f.toString());
-        }
-
-        System.out.println("Выберите файл, с которым Вы хотите работать:");
-        Scanner scanner = new Scanner(System.in);
-        String targetFile = rootPath + "/" + scanner.next();
-
-        System.out.println("Информация в файле:");
+        File file = printListOfFiles(rootPath);
+        String targetFile = getFileName(rootPath);
         readFile(file, targetFile);
-
         writeInFile(file, targetFile);
     }
 
+    /**
+     * Метод проверяет введённое в консоле название файла и в случае его наличия в каталоге, возвращает название.
+     *
+     * @param rootPath директория каталога файлов
+     * @return название существующего файла
+     */
+    private static String getFileName(String rootPath) {
+        System.out.println("Выберите файл, с которым Вы хотите работать:");
+        Scanner scanner = new Scanner(System.in);
+        String targetFile = rootPath + "/" + scanner.next();
+        while (!(new File(targetFile).exists())) {
+            System.out.println("Вы ввели несуществующий файл, попробуйте ещё раз:");
+            targetFile = rootPath + "/" + scanner.next();
+        }
+        return targetFile;
+    }
+
+    /**
+     * Метод печатает список файлов из директории
+     *
+     * @param rootPath директория каталога файлов
+     * @return File - путь к файлу(ам)
+     */
+    private static File printListOfFiles(String rootPath) {
+        System.out.println("Список файлов каталога:");
+        File file = new File(rootPath);
+        for (File f : Objects.requireNonNull(file.listFiles())) {
+            System.out.println("f.toString() = " + f.toString());
+        }
+        return file;
+    }
+
+    /**
+     * Метод читает даныне из файла
+     *
+     * @param file       путь к файлу
+     * @param targetFile имя файла
+     */
     private static void readFile(File file, String targetFile) {
+        System.out.println("Информация в файле:");
         if (file.exists()) {
             try (InputStreamReader isr = new InputStreamReader(
                     new FileInputStream(targetFile))) {
@@ -41,6 +69,12 @@ public class MainApp {
         }
     }
 
+    /**
+     * Метод пишет в файл введённую пользователем в консоле строку с данными.
+     *
+     * @param file       путь к файлу
+     * @param targetFile имя файла
+     */
     private static void writeInFile(File file, String targetFile) {
         System.out.println("Введите текст для записи в файл:");
         Scanner scanner1 = new Scanner(System.in);
@@ -54,7 +88,6 @@ public class MainApp {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("Теперь в файле записано:");
         readFile(file, targetFile);
     }
 }
